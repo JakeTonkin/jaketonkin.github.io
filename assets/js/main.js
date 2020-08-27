9d49026d278c3156bef0302e45b5a3900e2320d3
JakeTonkin <jcton2@student.monash.edu>
2020-08-27 20:21:34 +1000

    Final Home + About

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,38 @@
 $(document).ready(function(){
 
 var word1 = [
-	"Apple",
-	"Peach",
-	"Orange"
+	"monkey",
+	"chicken",
+	"cat",
+	"dog",
+	"fish",
+	"sheep",
+	"t-rex"
 ];
 
 var randomWord1 = word1[Math.floor(Math.random()*word1.length)];
 
 var word2 = [
-  "yell",
-  "dance",
-  "draw"
+  "toothbrush",
+  "phone",
+  "hammer",
+  "car",
+  "computer",
+  "shoe",
+  "trampoline"
 ];
 
 var randomWord2 = word2[Math.floor(Math.random()*word2.length)];
 
 var word3 = [
-  "red",
-  "blue",
-  "yellow"
+  "fuzzy",
+  "smelly",
+  "wooly",
+  "bouncy",
+  "wet",
+  "gooey",
+  "sticky",
+
 ];
 
 var randomWord3 = word3[Math.floor(Math.random()*word3.length)];
